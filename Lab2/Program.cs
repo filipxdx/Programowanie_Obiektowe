6ddd1b7d72ea78d22cc2a0ff0af537d417ce44a9
72441 <72441@RA117B-15>
2025-11-10 13:32:24 +0100

    commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+class Zwierze
+{
+    protected string nazwa;
+
+    public Zwierze(string nazwa)
+    {
+        this.nazwa = nazwa;
+    }
+
+    public virtual void DajGlos()
+    {
+        Console.WriteLine("...");
+    }
+}
+
+class Pies : Zwierze
+{
+    public Pies(string nazwa) : base(nazwa) { }
+
+    public override void DajGlos()
+    {
+        Console.WriteLine($"{nazwa} robi woof woof!");
+    }
+}
+
+class Kot : Zwierze
+{
+    public Kot(string nazwa) : base(nazwa) { }
+
+    public override void DajGlos()
+    {
+        Console.WriteLine($"{nazwa} robi miau miau!");
+    }
+}
+
+class Waz : Zwierze
+{
+    public Waz(string nazwa) : base(nazwa) { }
+
+    public override void DajGlos()
+    {
+        Console.WriteLine($"{nazwa} robi ssssssss!");
+    }
+}
+
+class Program
+{
+    public static void PowiedzCos(Zwierze zwierze)
+    {
+        zwierze.DajGlos();
+        Console.WriteLine($"Typ obiektu: {zwierze.GetType().Name}\n");
+    }
+
+    static void Main(string[] args)
+    {
+        Zwierze zwierze = new Zwierze("Zwierzak");
+        Pies pies = new Pies("Reksio");
+        Kot kot = new Kot("Mruczek");
+        Waz waz = new Waz("Kaa");
+
+        PowiedzCos(zwierze);
+        PowiedzCos(pies);
+        PowiedzCos(kot);
+        PowiedzCos(waz);
+
+        Piekarz piekarz = new Piekarz();
+        piekarz.Pracuj();
+
+
+        Console.WriteLine();
+
+        Console.WriteLine("Tworzenie A:");
+        A a = new A();
+
+        Console.WriteLine("\nTworzenie B:");
+        B b = new B();
+
+        Console.WriteLine("\nTworzenie C:");
+        C c = new C();
+    }
+}
+
+abstract class Pracownik
+{
+    public abstract void Pracuj();
+}
+
+class Piekarz : Pracownik
+{
+    public override void Pracuj()
+    {
+        Console.WriteLine("Trwa pieczenie...");
+    }
+}
+
+class A
+{
+    public A()
+    {
+        Console.WriteLine("To jest konstruktor A");
+    }
+}
+
+class B : A
+{
+    public B() : base()
+    {
+        Console.WriteLine("To jest konstruktor B");
+    }
+}
+
+class C : B
+{
+    public C() : base()
+    {
+        Console.WriteLine("To jest konstruktor C");
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+public interface IModular
+{
+    double Module();
+}
+
+public class ComplexNumber : ICloneable, IEquatable<ComplexNumber>, IModular
+{
+    private double re;
+    private double im;
+
+    public double Re
+    {
+        get { return re; }
+        set { re = value; }
+    }
+
+    public double Im
+    {
+        get { return im; }
+        set { im = value; }
+    }
+
+    public ComplexNumber(double re, double im)
+    {
+        this.re = re;
+        this.im = im;
+    }
+
+    public override string ToString()
+    {
+        string znak = im >= 0 ? "+" : "-";
+        return $"{re} {znak} {Math.Abs(im)}i";
+    }
+
+    public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(a.re + b.re, a.im + b.im);
+    }
+
+    public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+    {
+        return new ComplexNumber(a.re - b.re, a.im - b.im);
+    }
+
+    public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+    {
+        double real = a.re * b.re - a.im * b.im;
+        double imag = a.re * b.im + a.im * b.re;
+        return new ComplexNumber(real, imag);
+    }
+
+    public static ComplexNumber operator -(ComplexNumber a)
+    {
+        return new ComplexNumber(a.re, -a.im);
+    }
+
+    public object Clone()
+    {
+        return new ComplexNumber(this.re, this.im);
+    }
+
+    public bool Equals(ComplexNumber other)
+    {
+        if (other == null) return false;
+        return this.re == other.re && this.im == other.im;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is ComplexNumber other)
+            return Equals(other);
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return re.GetHashCode() ^ im.GetHashCode();
+    }
+
+    public static bool operator ==(ComplexNumber a, ComplexNumber b)
+    {
+        if (ReferenceEquals(a, b)) return true;
+        if ((object)a == null || (object)b == null) return false;
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(ComplexNumber a, ComplexNumber b)
+    {
+        return !(a == b);
+    }
+
+    public double Module()
+    {
+        return Math.Sqrt(re * re + im * im);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Test klasy ComplexNumber ===\n");
+
+        ComplexNumber z1 = new ComplexNumber(3, 4);
+        ComplexNumber z2 = new ComplexNumber(1, -2);
+
+        Console.WriteLine($"z1 = {z1}");
+        Console.WriteLine($"z2 = {z2}\n");
+
+        ComplexNumber suma = z1 + z2;
+        Console.WriteLine($"z1 + z2 = {suma}");
+
+        ComplexNumber roznica = z1 - z2;
+        Console.WriteLine($"z1 - z2 = {roznica}");
+
+        ComplexNumber iloczyn = z1 * z2;
+        Console.WriteLine($"z1 * z2 = {iloczyn}");
+
+        ComplexNumber sprzezenie = -z1;
+        Console.WriteLine($"Sprzężenie z1 = {sprzezenie}");
+
+        Console.WriteLine($"|z1| = {z1.Module():F2}");
+
+        ComplexNumber kopia = (ComplexNumber)z1.Clone();
+        Console.WriteLine($"Kopia z1 = {kopia}");
+
+        ComplexNumber z3 = new ComplexNumber(3, 4);
+        Console.WriteLine($"z1 == z3 ? {z1 == z3}");
+        Console.WriteLine($"z1 != z2 ? {z1 != z2}");
+        Console.WriteLine($"z1.Equals(z3)? {z1.Equals(z3)}");
+    }
+}
